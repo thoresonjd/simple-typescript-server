d19873fb280488d85df2a6c23231580948f5930e
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-06-25 10:30:40 -0700

    Update Server.ts

--- Server.ts
+++ Server.ts
@@ -2,4 +2,4 @@ import { App } from './App';
 
 const server: any = new App().getApp();
 const PORT: number = 2022;
-server.listen(PORT, () => console.log(`apitest listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
